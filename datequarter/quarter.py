df712829c24c285ab20bb2c85a8b13835f083b4d
Matt Sharp <matthew.sharp3@gmail.com>
2024-02-20 22:28:59 -0700

    feat: added days_in_quarter, days_active, percent_active functions refactor: changed type(obj) == obj to use isinstance() refactor: cleaned up instance check logic refactor: added arithmetic error messages style: formatted using black docs: added typehints and function notes

--- datequarter/quarter.py
+++ datequarter/quarter.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from typing import Tuple, List, Generator
 
 
 class DateQuarter:
@@ -9,85 +8,58 @@ class DateQuarter:
     _quarter: int = 0
 
     def __init__(self, year: int, quarter: int):
-        year = year + (quarter-1) // 4
-        quarter = (quarter-1) % 4 + 1
+        year = year + (quarter - 1) // 4
+        quarter = (quarter - 1) % 4 + 1
 
         self._year = year
         self._quarter = quarter
 
     @classmethod
-    def from_date(cls, date):
-        return cls(date.year, ((date.month-1)//3)+1)
+    def from_date(cls, date: datetime.date):
+        return cls(date.year, ((date.month - 1) // 3) + 1)
 
     def __repr__(self):
-        return f"<DateQuarter-{self._year},{self._quarter}Q>"
+        return f"<DateQuarter-Q{self._quarter}-{self._year}>"
 
-    def __gt__(self, other):
-        if type(other) == datetime.date:
-            return self.__gt__(DateQuarter.from_date(other))
-        elif type(other) == DateQuarter:
-            if self._year > other._year:
-                return True
-            elif self._year == other._year and self._quarter > other._quarter:
-                return True
+    def __str__(self):
+        return f"Q{self._quarter}-{self._year}"
 
-            return False
-        else:
-            raise ArithmeticError()
-
-    def __lt__(self, other):
-        if type(other) == datetime.date:
-            return self.__lt__(DateQuarter.from_date(other))
-        elif type(other) == DateQuarter:
-            if self._year < other._year:
-                return True
-            elif self._year == other._year and self._quarter < other._quarter:
-                return True
-        else:
-            raise ArithmeticError()
-
-        return False
-
-    def __contains__(self, item):
-        if type(item) == datetime.date:
-            return self.__eq__(DateQuarter.from_date(item))
-        else:
-            raise ArithmeticError()
+    def __contains__(self, item: datetime.date):
+        return self.__eq__(DateQuarter.from_date(item))
 
     def __eq__(self, other):
-        if type(other) == DateQuarter:
-            if self._year == other._year and self._quarter == other._quarter:
-                return True
-        else:
-            raise ArithmeticError()
+        if isinstance(other, DateQuarter):
+            return self._year == other._year and self._quarter == other._quarter
+        raise ArithmeticError(f"Cannot determine equlity with type {type(other)}")
 
-        return False
+    def __gt__(self, other):
+        if isinstance(other, datetime.date):
+            print("is datetime.date")
+            return self.__gt__(DateQuarter.from_date(other))
+        if isinstance(other, DateQuarter):
+            return self._year > other._year or (self._year == other._year and self._quarter > other._quarter)
+        raise ArithmeticError(f"Cannot determine equlity with type {type(other)}")
+
+    def __lt__(self, other):
+        if isinstance(other, datetime.date):
+            return self.__lt__(DateQuarter.from_date(other))
+        if isinstance(other, DateQuarter):
+            return self._year < other._year or (self._year == other._year and self._quarter < other._quarter)
+        raise ArithmeticError(f"Cannot determine equlity with type {type(other)}")
 
     def __ge__(self, other):
-        if type(other) == datetime.date:
+        if isinstance(other, datetime.date):
             return self.__ge__(DateQuarter.from_date(other))
-        elif type(other) == DateQuarter:
-            if self._year > other._year:
-                return True
-            elif self._year == other._year and self._quarter >= other._quarter:
-                return True
-        else:
-            raise ArithmeticError()
-
-        return False
+        if isinstance(other, DateQuarter):
+            return self._year > other._year or (self._year == other._year and self._quarter >= other._quarter)
+        raise ArithmeticError(f"Cannot determine equlity with type {type(other)}")
 
     def __le__(self, other):
-        if type(other) == datetime.date:
+        if isinstance(other, datetime.date):
             return self.__le__(DateQuarter.from_date(other))
-        elif type(other) == DateQuarter:
-            if self._year < other._year:
-                return True
-            elif self._year == other._year and self._quarter <= other._quarter:
-                return True
-        else:
-            raise ArithmeticError()
-
-        return False
+        if isinstance(other, DateQuarter):
+            return self._year < other._year or (self._year == other._year and self._quarter <= other._quarter)
+        raise ArithmeticError(f"Cannot determine equlity with type {type(other)}")
 
     def __getitem__(self, item):
         if item == 0:
@@ -98,20 +70,16 @@ class DateQuarter:
             raise KeyError()
 
     def __add__(self, other):
-        if type(other) == int:
-            return DateQuarter(self._year, self._quarter + other)
-        else:
-            raise ArithmeticError()
+        return DateQuarter(self._year, self._quarter + other)
 
     def __sub__(self, other):
-        if type(other) == int:
+        if isinstance(other, int):
             return DateQuarter(self._year, self._quarter - other)
-        elif type(other) == DateQuarter:
+        if isinstance(other, DateQuarter):
             quarter = (self._year - other._year) * 4
-            quarter += (self._quarter - other._quarter)
+            quarter += self._quarter - other._quarter
             return quarter
-        else:
-            raise ArithmeticError()
+        raise ArithmeticError(f"Cannot subtract type {type(other)}")
 
     def year(self) -> int:
         return self._year
@@ -119,28 +87,60 @@ class DateQuarter:
     def quarter(self) -> int:
         return self._quarter
 
-    def days(self) -> List[datetime.date]:
-        start = self.start_date()
-        end = self.end_date()
-        curr = start
-        while curr <= end:
-            yield curr
-            curr = curr + datetime.timedelta(days=1)
-
     def start_date(self) -> datetime.date:
-        return datetime.date(year=self._year, month=(self._quarter-1)*3+1, day=1)
+        return datetime.date(year=self._year, month=(self._quarter - 1) * 3 + 1, day=1)
 
     def end_date(self) -> datetime.date:
-        return (self+1).start_date() - datetime.timedelta(days=1)
+        return (self + 1).start_date() - datetime.timedelta(days=1)
+
+    def days_in_quarter(self) -> int:
+        """
+        Return total number of days in quarter
+        """
+        return ((self + 1).start_date() - self.start_date()).days
+
+    def days_active(self, start_or_end_date: datetime.date, is_start_date: bool = False) -> int:
+        """
+        Return total number of days in quarter before or after a date.\n
+        Inclusive of start / end date\n
+        When <is_start_date> = True will return the days *after* the provided date
+        """
+        if is_start_date:
+            return (self.end_date() - start_or_end_date).days + 1
+        return (start_or_end_date - self.start_date()).days + 1
+
+    def percent_active(self, start_or_end_date: datetime.date, is_start_date: bool = False) -> float:
+        """
+        Return percentage of quarter before or after a date.\n
+        Inclusive of start / end date\n
+        When <is_start_date> = True will return the days *after* the provided date\n
+        See also: active_days_in_quarter()
+        """
+        return min(round(self.days_active(start_or_end_date, is_start_date) / self.days_in_quarter(), 4), 1.0000)
+
+    def days(self):
+        """
+        Yield datetime.date for each day in quarter
+        """
+        start = self.start_date()
+        end = self.end_date()
+        current = start
+        while current <= end:
+            yield current
+            current = current + datetime.timedelta(days=1)
 
     @classmethod
     def between(cls, start: "DateQuarter", end: "DateQuarter", include_last: bool = False):
+        """
+        Yield DateQuarter for each quarter between start and end quarter.\n
+        When <include_last> = True will include end quarter
+        """
         delta = 1 if start < end else -1
 
-        curr = start
-        while curr != end:
-            yield curr
-            curr += delta
+        current = start
+        while current != end:
+            yield current
+            current += delta
 
         if include_last:
             yield end
